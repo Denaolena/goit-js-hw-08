d6b24f4304bdb7435ad0b83253d595d387a82b94
olena <denaolena@gmail.com>
2023-04-04 22:15:12 +0300

    додає const data, прибирає refs.form.addEventListener

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,24 +7,23 @@ const formData = {};
 const refs = {
   form: document.querySelector('.feedback-form'),
   email: document.querySelector('input'),
-
   textarea: document.querySelector('textarea'),
 };
 refs.form.addEventListener('submit', onFormSubmit);
-refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+refs.form.addEventListener('input', throttle(onInput, 500));
 
-refs.form.addEventListener('input', e => {
-  //   console.log(e.target.name);
-  //   console.log(e.target.value);
-  formData[e.target.name] = e.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+// refs.form.addEventListener('input', e => {
+//     console.log(e.target.name);
+//     console.log(e.target.value);
+//   formData[e.target.name] = e.target.value;
+//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 
-  const saveData = localStorage.getItem(STORAGE_KEY);
-  //   console.log('saveData', saveData);
+//   const saveData = localStorage.getItem(STORAGE_KEY);
+//     console.log('saveData', saveData);
 
-  const parseData = JSON.parse(saveData);
-  //   console.log('parseData', parseData);
-});
+//   const parseData = JSON.parse(saveData);
+//     console.log('parseData', parseData);
+// });
 
 populateTextarea();
 
@@ -36,9 +35,12 @@ function onFormSubmit(evt) {
   localStorage.removeItem(STORAGE_KEY);
 }
 
-function onTextareaInput(evt) {
-  const message = evt.target.value;
-  localStorage.setItem(STORAGE_KEY, message);
+function onInput(evt) {
+  const data = {
+    email: refs.email.value,
+    message: refs.textarea.value,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
 }
 
 function populateTextarea() {
